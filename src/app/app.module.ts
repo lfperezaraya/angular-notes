905cb50b3043e37d2595a1cdb808d87b77ba56d3
Luis Fernando Perez Araya <lperez-as@sca-lp295.avantek-sc.avantica.avanticatec.net avantica.avanticatec.net avanticape.avantica.avanticatec.net avanticabol.avantica.avanticatec.net avantica.net>
2018-08-21 10:15:45 -0600

    feat: Routing set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -12,6 +14,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
+    AuthModule,
     SharedModule
   ],
   providers: [],
